935f98da250d82b2bcdc11075e2560798f0d0827
Amir <amirtaja@yahoo.com>
2022-11-01 12:25:02 +0330

    d

--- pages/panel.vue
+++ pages/panel.vue
@@ -1,13 +1,10 @@
 <template>
   <div id="root">
-    <v-card >
-     
-
+    <v-card>
       <v-navigation-drawer
- 
-        class="primary accent-4"
+        class="danger accent-4"
         height="100vh"
-        style="border-top-right-radius: 0;border-bottom-right-radius: 0;"
+        style="border-top-right-radius: 0; border-bottom-right-radius: 0"
         dark
         absolute
         right
@@ -17,9 +14,9 @@
         permanent
       >
         <v-list-item class="px-2">
-       
-
-          <v-list-item-title  v-if="!mini" class="text-right">{{ $auth.user.name }}</v-list-item-title>
+          <v-list-item-title v-if="!mini" class="text-right">{{
+            $auth.user.name
+          }}</v-list-item-title>
           <v-list-item-avatar>
             <v-img :src="$auth.user.image"></v-img>
           </v-list-item-avatar>
@@ -31,11 +28,17 @@
         <v-divider></v-divider>
 
         <v-list dense>
-          <v-list-item :to="item.link" :nuxt="true" v-for="item in items" :key="item.title" link>
-           
-      
+          <v-list-item
+            :to="item.link"
+            :nuxt="true"
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
             <v-list-item-content v-if="!mini">
-              <v-list-item-title  class="text-right">{{ item.title }}</v-list-item-title>
+              <v-list-item-title class="text-right">{{
+                item.title
+              }}</v-list-item-title>
             </v-list-item-content>
             <v-list-item-icon>
               <v-icon>{{ item.icon }}</v-icon>
@@ -44,10 +47,7 @@
         </v-list>
         <template v-slot:append>
           <v-list dense>
-       
             <v-list-item @click="logout" link>
-            
-
               <v-list-item-content v-if="!mini">
                 <v-list-item-title class="text-right">خروج</v-list-item-title>
               </v-list-item-content>
@@ -61,33 +61,31 @@
     </v-card>
 
     <div class="container">
-      
-      <nuxt-child/>
-  
-      
+      <nuxt-child />
     </div>
-   
   </div>
 </template>
 <script>
 export default {
-   loading: {
-    color: 'blue',
-    height: '5px'
+  loading: {
+    color: "blue",
+    height: "5px",
   },
   middleware: ["authenticated"],
 
   name: "Dashboard",
 
-
   data() {
     return {
       drawer: true,
       items: [
-        { title: "داشبورد", icon: "mdi-view-dashboard" , link: "/panel/dashboard"},
-        { title: "آزمون های من", icon: "mdi-book" , link: "/panel/exams"},
-        { title: "حساب کاربری", icon: "mdi-account" , link: "/panel/account"},
-     
+        {
+          title: "داشبورد",
+          icon: "mdi-view-dashboard",
+          link: "/panel/dashboard",
+        },
+        { title: "آزمون های من", icon: "mdi-book", link: "/panel/exams" },
+        { title: "حساب کاربری", icon: "mdi-account", link: "/panel/account" },
       ],
       mini: true,
     };
@@ -103,28 +101,37 @@ export default {
 };
 </script>
 <style>
-.inner-card{
+.inner-card {
   padding: 1rem 3rem 3rem 2rem;
 }
-@media screen and (max-width: 600px) {
-  .inner-card{
-  padding: 1rem 1rem 1rem 1.5rem;
-}
-}
-.container{
+.container {
   direction: rtl;
   text-align: right;
-  margin-top:100px;
+  margin-top: 50px;
   padding-right: 70px;
   padding-bottom: 50px;
+}
+@media screen and (max-width: 600px) {
+  .inner-card {
+    padding: 1rem 1rem 1rem 1.5rem;
+  }
+  .container{
+
+  margin-top:20px;
+
 
 }
-#root{
-  background-color: #f5f5f5;min-height: 100vh;
 }
-h1{
+
+#root {
+  background-color: #f5f5f5;
+  min-height: 100vh;
+}
+h1 {
+  font-size: 25px;
   font-family: morabba;
-    font-weight: 800;
-    line-height: 1.7;
-    margin-top: 13px;
-}</style>
+  font-weight: 800;
+  line-height: 1.7;
+  margin-top: 13px;
+}
+</style>
--- pages/panel/account.vue
+++ pages/panel/account.vue
@@ -1,58 +1,93 @@
 <template>
   <div>
-    <v-card class="inner-card">
+          <h1 class="mb-1">حساب کاربری</h1>
+          <div class="text-gray mb-4" style="font-size:14px; color:gray"> از طریق فرم زیر نام خود را تغییر دهید یا عکس کنید</div>
 
-      <h1>
-        حساب کاربری
-      </h1>
+    <v-card style="border-radius:8px" class="inner-card">
 
-      <v-row class="mt-4">
-
-        <v-col cols="12" sm="6" md="4">
-          <v-text-field @keyup.13="updateAccount" :rules="[
-            () => !!name || 'این فیلد الزامی است',
-            () => name.search('^[\u0600-\u06FF\uFB8A\u067E\u0686\u06AF\u200C\u200F ]+$') == 0 || 'نام خود را به فارسی وارد کنید',
-          ]" label="نام و نام خانوادگی" outlined reverse dense v-model="name"></v-text-field>
-        </v-col>
-      </v-row>
       <v-row>
-        <v-col cols="12" sm="6" md="4">
-          <template>
-            <v-file-input @change="getFile" v-model="file" :rules="[
-              () => !file || file.size < (6 * 1024 * 1024) || 'حجم تصویر باید کمتر از 6 مگابایت باشد',
-            ]" accept="image/png, image/jpeg,  image/jpg" reverse placeholder="یک تصویر انتخاب کنید ..."
-              prepend-icon="mdi-camera" label="تصویر پروفایل"></v-file-input>
-          </template>
+        <v-col cols="12" sm="6">
+          <v-row class="mt-4">
+            <v-col  cols="12" md="8">
+              <v-text-field
+                @keyup.13="updateAccount"
+                :rules="[
+                  () => !!name || 'این فیلد الزامی است',
+                  () =>
+                    name.search(
+                      '^[\u0600-\u06FF\uFB8A\u067E\u0686\u06AF\u200C\u200F ]+$'
+                    ) == 0 || 'نام خود را به فارسی وارد کنید',
+                ]"
+                label="نام و نام خانوادگی"
+                outlined
+                reverse
+                dense
+                v-model="name"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="8">
+              <template>
+                <v-file-input
+                  @change="getFile"
+                  v-model="file"
+                  :rules="[
+                    () =>
+                      !file ||
+                      file.size < 6 * 1024 * 1024 ||
+                      'حجم تصویر باید کمتر از 6 مگابایت باشد',
+                  ]"
+                  accept="image/png, image/jpeg,  image/jpg"
+                  reverse
+                  placeholder="یک تصویر انتخاب کنید ..."
+                  prepend-icon="mdi-camera"
+                  label="تصویر پروفایل"
+                ></v-file-input>
+              </template>
+            </v-col>
+            <v-col cols="12">
+              <v-img
+                v-if="uploaded"
+                style="border-radius: 50%"
+                height="140"
+                :src="uploaded"
+                width="140"
+              ></v-img>
+            </v-col>
+          </v-row>
+          <v-col  cols="12" sm="2" class="mt-2" style="padding:0">
+            <v-btn
+              class="m-0 col-12" style="padding:0"
+            
 
-
+              :loading="loading"
+              :disabled="
+                loading ||
+                !name ||
+                name.search(
+                  '^[\u0600-\u06FF\uFB8A\u067E\u0686\u06AF\u200C\u200F ]+$'
+                ) == -1
+              "
+              color="info"
+              @click="updateAccount"
+            >
+              ثبت
+              <template v-slot:loader>
+                <span class="custom-loader">
+                  <v-icon light>mdi-cached</v-icon>
+                </span>
+              </template>
+            </v-btn>
+          </v-col>
         </v-col>
-        <v-col cols="12" sm="6" md="4">
-          <v-img v-if="uploaded" style="border-radius:50%" height="140" :src="uploaded" width="140"></v-img>
-        </v-col>
 
-
-      </v-row>
-      <v-row>
-        <v-btn class="ma-2" :loading="loading"
-          :disabled="loading || !name || name.search('^[\u0600-\u06FF\uFB8A\u067E\u0686\u06AF\u200C\u200F ]+$') == -1"
-          color="info" @click="updateAccount">
-          ثبت
-          <template v-slot:loader>
-            <span class="custom-loader">
-              <v-icon light>mdi-cached</v-icon>
-            </span>
-          </template>
-        </v-btn>
+        <v-col cols="12" sm="6">
+                <img style="float:left" src="/assets/illu/settings.png">
+ </v-col>
       </v-row>
     </v-card>
-
-    <div class="mt-5">
-      <img style="float:left" src="/assets/illu/settings.png">
-    </div>
   </div>
-
-
-
 </template>
 <script>
 export default {
@@ -62,10 +97,8 @@ export default {
       file: "",
       loading: false,
       name: this.$auth.user.name,
-
-    }
-  }
-  ,
+    };
+  },
   methods: {
     getBase64(file) {
       return new Promise((resolve, reject) => {
@@ -76,59 +109,52 @@ export default {
       });
     },
     async getFile(file) {
-
-
-
       try {
         this.uploaded = await this.getBase64(file);
         let formData = new FormData();
-        formData.append('file', file);
-        var upload = await this.$axios.post("users/change-image", formData, {
-          headers: {
-            'Content-Type': "multipart/form-data; charset=utf-8; boundary=" + Math.random().toString().substr(2)
-          }
-        }).then(res => res.data)
+        formData.append("file", file);
+        var upload = await this.$axios
+          .post("users/change-image", formData, {
+            headers: {
+              "Content-Type":
+                "multipart/form-data; charset=utf-8; boundary=" +
+                Math.random().toString().substr(2),
+            },
+          })
+          .then((res) => res.data);
         if (upload.success) {
-
-          this.$auth.fetchUser()
+          this.$auth.fetchUser();
           this.$toast.success("تصویر با موفقیت آپلود شد");
-
         } else {
           this.$toast.error(upload.msg);
         }
       } catch (er) {
         // this.$toast.error("خطایی در آپلود تصویر رخ داد لطفا بعدا تلاش کنید.");
       }
-
-
     },
     async updateAccount() {
-      this.loading = true
+      this.loading = true;
       try {
-        var req = await this.$axios.post(`users/update`, { name: this.name }).then(res => res.data);
+        var req = await this.$axios
+          .post(`users/update`, { name: this.name })
+          .then((res) => res.data);
         if (req.success) {
-          this.$auth.fetchUser()
+          this.$auth.fetchUser();
           this.$toast.success("اطلاعات با موفقیت بروزرسانی شد");
-
         } else {
           this.$toast.error(req.msg);
         }
       } catch (er) {
-
         this.$toast.error("خطایی رخ داد لطفا بعدا تلاش کنید.");
-
       }
 
-
-      this.loading = false
-
+      this.loading = false;
     },
-
   },
 
   name: "account",
   transitions: "home",
-}
+};
 </script>
 <style>
 .custom-loader {
