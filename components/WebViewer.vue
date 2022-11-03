a78fd1a6d6007bcb9dc395904f5a63601c4d3dd6
Your Name <amirtaja@yahoo.com>
2022-11-03 17:28:50 +0330

    ff

--- components/WebViewer.vue
+++ components/WebViewer.vue
@@ -1,6 +1,6 @@
 <template>
 
-<iframe style="margin-top:10px" frameborder="0" allowfullscreen width="100%" height="600" type="application/pdf" :src="url "/>
+<iframe style="margin-top:10px" frameborder="0" allowfullscreen width="100%" height="600" type="application/pdf" :src="'https://docs.google.com/gview?embedded=true&url=' + url"/>
     
 </template>
   
