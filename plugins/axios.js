c37094173c8a51f83641958bf895712e4ecda933
Your Name <amirtaja@yahoo.com>
2022-11-08 20:44:29 +0330

    f

---
+++ plugins/axios.js
@@ -0,0 +1,10 @@
+export default function ({ $axios, $auth }) {
+    $axios.onError((error) => {
+      if (error.response.status === 401) {
+        if($auth.user?.phone){
+            $auth.refreshTokens()
+
+        }
+      }
+    })
+  }
